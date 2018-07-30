746834a599d5691e86e234014bd24c7621e60416
jean.visoski <jean.visoski_est@compasso.com.br>
2018-07-30 11:32:55 -0300

    2048 funcionando

--- js/script.js
+++ js/script.js
@@ -1,190 +1,519 @@
-/* MY STYLES */
-
-body {
-	font-family: 'Open Sans', sans-serif;
-	background-color: #FAF8EF;
-}
-
-h1 {
-	font-size: 5rem;
-	text-align: center;
-	padding: 20px;
-	color: #776E65;
-}
-
-#scoreboard {
-	width: 70vw;
-	margin: 0 auto;
-}
-
-#scoreboard::after {
-	content: "";
-	display: block;
-	clear: both;
-}
-
-.score {
-	float: left;
-	width: 33vw;
-	height: 50px;
-	background-color: #EEE4DA;
-	color: #776E65;
-	text-align: center;
-	border-radius: 5px;
-}
-
-.score:last-of-type {
-	margin-left: 4vw;
-}
-
-.actualscore {
-	font-size: 1.5rem;
-	padding-top: 5px;
-}
-
-#container {
-  width: 80vw;
-  height: 80vw;
-  background: #BBADA0;
-	box-sizing: border-box;
-	margin: 20px auto;
-	border-radius: 5px;
-	position: relative;
-}
-
-.cell {
-	width: 16vw;
-	height: 16vw;
-	background-color: rgba(238, 228, 218, 0.35);
-	float: left;
-	margin-left: 3.2vw;
-	margin-top: 3.2vw;
-	line-height: 16vw;
-	text-align: center;
-	font-size: 1.5rem;
-	border-radius: 5px;
-}
-
-#won, #lost {
-	position: absolute;
-	top: 3.2vw;
-	left: 3.2vw;
-	right: 3.2vw;
-	bottom: 3.2vw;
-	background-color: rgba(0, 0, 0, 0.5);
-	color: #FFF;
-	text-align: center;
-	line-height: 73.6vw;
-	border-radius: 5px;
-	font-size: 2rem;
-	display: none;
-}
-
-/* Large devices (desktops, 62em and up) */
-@media (min-width: 56em) {
-	h1 {
-		padding: 50px;
+function Game () {
+	this.grid = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0,    0],[0, 0, 0, 0]];
+	this.score = 0;
+	this.bestscore;
+	this.init();
+  }
+  
+  Game.prototype.init = function () {
+	this.addRandomCell();
+	this.addRandomCell();
+	this.printCells();
+	if (localStorage.getItem('bestscore')) {
+	  this.bestscore = localStorage.getItem('bestscore');
+	} else {
+	  this.bestscore = 0;
 	}
-
-	#scoreboard {
-		width: 320px;
+	this.printScore();
+	this.move();
+  };
+  
+  Game.prototype.getRandom2 = function () {
+	return Math.random() < 0.9 ? 2 : 4;
+  };
+  
+  Game.prototype.getRandom4 = function () {
+	return Math.floor(Math.random() * 4);
+  };
+  
+  Game.prototype.addRandomCell = function () {
+	var line = this.getRandom4();
+	var column = this.getRandom4();
+  
+	if (this.grid[line][column] === 0) {
+	  this.grid[line][column] = this.getRandom2();
+	} else {
+	  this.addRandomCell();
 	}
-
-	.score {
-		width: 150px;
+  };
+  
+  Game.prototype.resetView = function () {
+	var i,
+		j,
+		cell = document.getElementsByClassName('cell');
+  
+		i = 0;
+		while (i < this.grid.length) {
+		  j = 0;
+		  while (j < this.grid[i].length) {
+			cell[i*4 + j].innerHTML = '';
+			cell[i*4 + j].className = 'cell';
+			j += 1;
+		  }
+		  i += 1;
+		}
+  };
+  
+  Game.prototype.printCells = function () {
+	var i,
+		j,
+		cell = document.getElementsByClassName('cell');
+  
+	this.resetView();
+  
+		i = 0;
+		while (i < this.grid.length) {
+		  j = 0;
+		  while (j < this.grid[i].length) {
+			if (this.grid[i][j] > 0) {
+			  cell[i*4 + j].innerHTML = this.grid[i][j];
+			}
+			j += 1;
+		  }
+		  i += 1;
+		}
+	this.addClasses();
+  };
+  
+  Game.prototype.printScore = function () {
+	var score = document.getElementsByClassName('actualscore')[0];
+	var bestscore = document.getElementsByClassName('actualscore')[1];
+  
+	score.innerHTML = this.score;
+	if (this.score > this.bestscore) {
+	  bestscore.innerHTML = this.score;
+	  localStorage['bestscore'] = this.score;
+	} else {
+	  bestscore.innerHTML = this.bestscore;
 	}
-
-	.score:last-of-type {
-		margin-left: 20px;
+  };
+  
+  Game.prototype.addClasses = function () {
+	var i,
+		j,
+		cell = document.getElementsByClassName('cell');
+  
+	i = 0;
+	while (i < 16) {
+	  j = 2;
+	  if (cell[i].innerHTML) {
+		while (j != cell[i].innerHTML) {
+		  j = j * 2;
+		}
+		cell[i].className = 'cell cell-' + j;
+	  }
+	  i += 1;
 	}
-
-	#container {
-		width: 500px;
-	  height: 500px;
+  };
+  
+  Game.prototype.moveLeft = function () {
+	var i,
+		j,
+		k,
+		canMove = false;
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  if (this.grid[i] != [0, 0, 0, 0]) {
+		k = 0;
+		while (k < this.grid[i].length) {
+		  j = 0;
+			while (j < this.grid[i].length - 1) {
+			  if (this.grid[i][j] === 0) {
+				this.grid[i][j] = this.grid[i][j+1];
+				this.grid[i][j+1] = 0;
+				if (this.grid[i][j] !== 0) {
+				  canMove = true;
+				}
+			  }
+			  j += 1;
+			}
+		  k += 1;
+		}
+	  }
+	  i += 1;
 	}
-
-	.cell {
-		width: 100px;
-		height: 100px;
-		margin-left: 20px;
-		margin-top: 20px;
-		line-height: 100px;
-		font-size: 2.5rem;
+  
+	return canMove;
+  
+  };
+  
+  Game.prototype.moveRight = function () {
+	var i,
+		j,
+		k,
+		canMove = false;
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  if (this.grid[i] != [0, 0, 0, 0]) {
+		k = 0;
+		while (k < this.grid[i].length) {
+		  j = this.grid.length - 1;
+			while (j > 0) {
+			  if (this.grid[i][j] === 0) {
+				this.grid[i][j] = this.grid[i][j-1];
+				this.grid[i][j-1] = 0;
+				if (this.grid[i][j] !== 0) {
+				  canMove = true;
+				}
+			  }
+			  j -= 1;
+			}
+		  k += 1;
+		}
+	  }
+	  i += 1;
 	}
-
-	.cell-1024, .cell-2048 {
-		font-size: 2rem !important;
+  
+	return canMove;
+  
+  };
+  
+  Game.prototype.moveUp = function () {
+	var i,
+		j,
+		k,
+		canMove = false;
+  
+	k = 0;
+	while (k < this.grid.length - 1) {
+	  i = 0;
+	  while (i < this.grid.length - 1) {
+		j = 0;
+		while (j < this.grid.length) {
+		  if (this.grid[i][j] === 0) {
+			this.grid[i][j] = this.grid[i+1][j];
+			this.grid[i+1][j] = 0;
+			if (this.grid[i][j] !== 0) {
+			  canMove = true;
+			}
+		  }
+		  j += 1;
+		}
+		i += 1;
+	  }
+	  k += 1;
 	}
-
-	#won, #lost {
-		top: 20px;
-		left: 20px;
-		right: 20px;
-		bottom: 20px;
-		line-height: 460px;
+  
+	return canMove;
+  
+  };
+  
+  Game.prototype.moveDown = function () {
+	var i,
+		j,
+		k,
+		canMove = false;
+  
+	k = 0;
+	while (k < this.grid.length - 1) {
+	  i = this.grid.length - 1;
+	  while (i > 0) {
+		j = 0;
+		while (j < this.grid.length) {
+		  if (this.grid[i][j] === 0) {
+			this.grid[i][j] = this.grid[i-1][j];
+			this.grid[i-1][j] = 0;
+			if (this.grid[i][j] !== 0) {
+			  canMove = true;
+			}
+		  }
+		  j += 1;
+		}
+		i -= 1;
+	  }
+	  k += 1;
 	}
-
-}
-
-/* CELLS STYLES */
-
-.cell-2, .cell-4 {
-	color: #776E65;
-}
-
-.cell-8, .cell-16, .cell-32, .cell-64,
-.cell-128, .cell-256, .cell-512, .cell-1024,
-.cell-2048 {
-	color: #F9F6F2;
-}
-
-.cell-2 {
-	background-color: #EEE4DA !important;
-}
-
-.cell-4 {
-	background-color: #EDE0C8 !important;
-}
-
-.cell-8 {
-	background-color: #F2B179 !important;
-}
-
-.cell-16 {
-	background-color: #F59563 !important;
-}
-
-.cell-32 {
-	background-color: #F67C5F !important;
-}
-
-.cell-64 {
-	background-color: #F65E3B !important;
-}
-
-.cell-128 {
-	background-color: #EDCF72 !important;
-	box-shadow: 0px 0px 30px 10px rgba(243, 215, 116, 0.24), 0px 0px 0px 1px rgba(255, 255, 255, 0.14) inset;
-}
-
-.cell-256 {
-	background-color: #EDCC61 !important;
-	box-shadow: 0px 0px 30px 10px rgba(243, 215, 116, 0.318), 0px 0px 0px 1px rgba(255, 255, 255, 0.192) inset;
-}
-
-.cell-512 {
-	background-color: #EDC850 !important;
-	box-shadow: 0px 0px 30px 10px rgba(243, 215, 116, 0.396), 0px 0px 0px 1px rgba(255, 255, 255, 0.24) inset;
-}
-
-.cell-1024 {
-	background-color: #EDC53F !important;
-	box-shadow: 0px 0px 30px 10px rgba(243, 215, 116, 0.475), 0px 0px 0px 1px rgba(255, 255, 255, 0.286) inset;
-	font-size: 1.25rem;
-}
-
-.cell-2048 {
-	background-color: #EDC22E !important;
-	box-shadow: 0px 0px 30px 10px rgba(243, 215, 116, 0.557), 0px 0px 0px 1px rgba(255, 255, 255, 0.333) inset;
-	font-size: 1.25rem;
-}
+  
+	return canMove;
+  
+  };
+  
+  Game.prototype.checkMatchesLeft = function () {
+	var i,
+		j,
+		canMerge = false;
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  j = 0;
+	  while (j < this.grid[i].length - 1) {
+		if (this.grid[i][j] !== 0 && this.grid[i][j] === this.grid[i][j+1]) {
+		  this.grid[i][j] = this.grid[i][j] * 2;
+		  this.score += this.grid[i][j];
+		  this.grid[i][j+1] = 0;
+		  canMerge = true;
+		}
+		j += 1;
+	  }
+	  i += 1;
+	}
+  
+	this.moveLeft();
+	return canMerge;
+  
+  };
+  
+  Game.prototype.checkMatchesRight = function () {
+	var i,
+		j,
+		canMerge = false;
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  j = this.grid.length - 1;
+	  while (j > 0) {
+		if (this.grid[i][j] !== 0 && this.grid[i][j] === this.grid[i][j-1]) {
+		  this.grid[i][j] = this.grid[i][j] * 2;
+		  this.score += this.grid[i][j];
+		  this.grid[i][j-1] = 0;
+		  canMerge = true;
+		}
+		j -= 1;
+	  }
+	  i += 1;
+	}
+  
+	this.moveRight();
+	return canMerge;
+  
+  };
+  
+  Game.prototype.checkMatchesUp = function () {
+	var i,
+		j,
+		canMerge = false;
+  
+	i = 0;
+	while (i < this.grid.length - 1) {
+	  j = 0;
+	  while (j < this.grid.length) {
+		if (this.grid[i][j] !== 0 && this.grid[i][j] === this.grid[i+1][j]) {
+		  this.grid[i][j] = this.grid[i][j] * 2;
+		  this.score += this.grid[i][j];
+		  this.grid[i+1][j] = 0;
+		  canMerge = true;
+		}
+		j += 1;
+	  }
+	  i += 1;
+	}
+  
+	this.moveUp();
+	return canMerge;
+  
+  };
+  
+  Game.prototype.checkMatchesDown = function () {
+	var i,
+		j,
+		canMerge = false;
+  
+	i = this.grid.length - 1;
+	while (i > 0) {
+	  j = 0;
+	  while (j < this.grid.length) {
+		if (this.grid[i][j] !== 0 && this.grid[i][j] === this.grid[i-1][j]) {
+		  this.grid[i][j] = this.grid[i][j] * 2;
+		  this.score += this.grid[i][j];
+		  this.grid[i-1][j] = 0;
+		  canMerge = true;
+		}
+		j += 1;
+	  }
+	  i -= 1;
+	}
+  
+	this.moveDown();
+	return canMerge;
+  
+  };
+  
+  Game.prototype.move = function () {
+  
+	var self = this;
+	var move,
+		check;
+  
+	document.addEventListener('keydown', function movements(e) {
+	  e = e || window.event;
+  
+	  if (e.keyCode == '37') {
+		move = self.moveLeft();
+		check = self.checkMatchesLeft();
+		if (move || check) {
+		  self.addRandomCell();
+		  self.printCells();
+		  self.printScore();
+		}
+	  }
+	  else if (e.keyCode == '38') {
+		move = self.moveUp();
+		check = self.checkMatchesUp();
+		if (move || check) {
+		  self.addRandomCell();
+		  self.printCells();
+		  self.printScore();
+		}
+	  }
+	  else if (e.keyCode == '39') {
+		move = self.moveRight();
+		check = self.checkMatchesRight();
+		if (move || check) {
+		  self.addRandomCell();
+		  self.printCells();
+		  self.printScore();
+		}
+	  }
+	  else if (e.keyCode == '40') {
+		move = self.moveDown();
+		check = self.checkMatchesDown();
+		if (move || check) {
+		  self.addRandomCell();
+		  self.printCells();
+		  self.printScore();
+		}
+	  }
+  
+	  self.checkIfLost();
+  
+	  if (self.checkIfWon()) {
+		console.log('gagné');
+		document.removeEventListener('keydown', movements);
+	  }
+  
+	});
+  
+	document.addEventListener('touchstart', handleTouchStart, false);
+	document.addEventListener('touchmove', handleTouchMove, false);
+  
+	var xDown = null;
+	var yDown = null;
+  
+	function handleTouchStart(evt) {
+		xDown = evt.touches[0].clientX;
+		yDown = evt.touches[0].clientY;
+	};
+  
+	function handleTouchMove(evt) {
+	  event.preventDefault();
+  
+	  self.checkIfLost();
+  
+	  if (self.checkIfWon()) {
+		document.removeEventListener('touchstart', handleTouchStart, false);
+	  }
+  
+		if ( ! xDown || ! yDown ) {
+			return;
+		}
+  
+		var xUp = evt.touches[0].clientX;
+		var yUp = evt.touches[0].clientY;
+  
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+  
+		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+			if ( xDiff > 0 ) {
+			  move = self.moveLeft();
+			  check = self.checkMatchesLeft();
+			  if (move || check) {
+				self.addRandomCell();
+				self.printCells();
+				self.printScore();
+			  }
+			} else {
+			  move = self.moveRight();
+			  check = self.checkMatchesRight();
+			  if (move || check) {
+				self.addRandomCell();
+				self.printCells();
+				self.printScore();
+			  }
+			}
+		} else {
+			if ( yDiff > 0 ) {
+			  move = self.moveUp();
+			  check = self.checkMatchesUp();
+			  if (move || check) {
+				self.addRandomCell();
+				self.printCells();
+				self.printScore();
+			  }
+			} else {
+			  move = self.moveDown();
+			  check = self.checkMatchesDown();
+			  if (move || check) {
+				self.addRandomCell();
+				self.printCells();
+				self.printScore();
+			  }
+			}
+		}
+		/* reset values */
+		xDown = null;
+		yDown = null;
+  
+	};
+  
+  };
+  
+  Game.prototype.checkIfWon = function () {
+	var i,
+		won = false,
+		div = document.getElementById('won');
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  if (this.grid[i].indexOf(2048) > -1) {
+		won = true;
+		div.style.display = 'block';
+		break;
+	  }
+	  i += 1;
+	}
+  
+	return won;
+  
+  };
+  
+  Game.prototype.checkIfLost = function () {
+	var i,
+		j,
+		lost = true,
+		div = document.getElementById('lost');
+  
+	i = 0;
+	while (i < this.grid.length - 1) {
+	  j = 0;
+	  while (j < this.grid.length) {
+		if (this.grid[i][j] === 0 || this.grid[i][j] === this.grid[i+1][j] || this.grid[i+1][j] === 0) {
+		  lost = false;
+		}
+		j += 1;
+	  }
+	  i += 1;
+	}
+  
+	i = 0;
+	while (i < this.grid.length) {
+	  j = 0;
+	  while (j < this.grid[i].length - 1) {
+		if (this.grid[i][j] === 0 || this.grid[i][j] === this.grid[i][j+1] || this.grid[i][j+1] === 0) {
+		  lost = false;
+		}
+		j += 1;
+	  }
+	  i += 1;
+	}
+  
+	if (lost) {
+	  div.style.display = 'block';
+	}
+  
+  };
+  
+  new Game();
+  
